f9585300e4f91eb937dbaeff4f13c518f16c2859
machajdik <manuel.machajdik@gmail.com>
2019-07-17 16:59:07 +0200

    1.5.7

--- LtcKomfortkasse/Controllers/Api/OrderId.php
+++ LtcKomfortkasse/Controllers/Api/OrderId.php
@@ -21,10 +21,13 @@ class Shopware_Controllers_Api_OrderId extends Shopware_Controllers_Api_Rest
     {
         $id = $this->Request()->getParam('id');
         $useNumberAsId = $this->Request()->getParam('useNumberAsId');
+        $subshops = $this->Request()->getParam('subshops');
 
         $sql = 'select id from s_order where ';
         $sql .= ($useNumberAsId == 'true' ? 'ordernumber' : 'id');
         $sql .= '=\'' . $id . '\'';
+        if ($subshops)
+            $sql .= ' and subshopID in (' . $subshops . ')';
 
         $id = Shopware()->Db()->fetchOne($sql);
         $data['id'] = ($id === false ? null : $id);
